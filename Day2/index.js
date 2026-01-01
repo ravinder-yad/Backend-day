4468564c5f1abc556ff9a3841b1817f47492aa4d
Ravinder Yadav <ravinderyadav092007@gmail.com>
2026-01-01 14:23:39 +0530

    Day 2 contribution after email fix

--- Day2/index.js
+++ Day2/index.js
@@ -1,11 +1,11 @@
 // create server in Node.js 
 
-let http = require("http");
+let http = require("http")
 
 let server = http.createServer((req, res) => {
     res.write("welcome to ws");
     res.end();
-});
+})
 
 server.listen(8000, () => {
     console.log("http://localhost:8000");
